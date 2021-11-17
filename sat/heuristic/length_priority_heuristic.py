33849438b85c11b3901f0ccaee95d7c03b3cad63
b0ster <bosterenborg@gmail.com>
2021-11-17 15:42:13 +0100

    Made algorithm bit more efficient

--- sat/heuristic/length_priority_heuristic.py
+++ sat/heuristic/length_priority_heuristic.py
@@ -8,18 +8,18 @@ class LengthPriorityHeuristic(Heuristic):
         return "length_priority_max"
 
     def select(self, problem: SATProblem, var_assignments: {}):
-        var_counts = {}
+        for v in problem.get_unit_variables():
+            if v not in var_assignments.keys():
+                return abs(v), False
+        lit_lengths = {}
         for c in problem.get_clauses().values():
             for lit in c:
-                var = abs(lit)
-                if var in var_counts:
-                    var_counts[var][lit] = 1 / len(c) if 1 / len(c) > var_counts[var][lit] else var_counts[var][lit]
+                if lit in lit_lengths:
+                    if 1 / len(c) > lit_lengths[lit]:
+                        lit_lengths[lit] = 1 / len(c)
                 else:
-                    var_counts[var] = {}
-                    var_counts[var][lit] = 1 / len(c)
-                    var_counts[var][-lit] = 0
-        vc_list = sorted(var_counts.items(), key=lambda x: max(x[1][x[0]], x[1][-x[0]]), reverse=True)
-        for i in vc_list:
-            if i[0] not in var_assignments.keys():
-                return i[0], i[1][i[0]] > i[1][-i[0]]
+                    lit_lengths[lit] = 1 / len(c)
+        if len(lit_lengths):
+            l = max(lit_lengths, key=lit_lengths.get)
+            return abs(l), l > 0
         raise Heuristic.no_var_left_exception
