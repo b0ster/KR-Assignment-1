8b1cd8b0f6396577e4e210f42890f46beda132d5
b0ster <bosterenborg@gmail.com>
2021-11-17 21:05:28 +0100

    Back to local state

--- sat/heuristic/one_sided_jeroslow_wang_heuristic.py
+++ sat/heuristic/one_sided_jeroslow_wang_heuristic.py
@@ -3,7 +3,6 @@ from util.sat_problem import SATProblem
 
 
 class OneSidedJeroslowWangHeuristic(Heuristic):
-    lit_weights = {}
 
     def name(self) -> str:
         return "1_sided_jw"
@@ -12,14 +11,15 @@ class OneSidedJeroslowWangHeuristic(Heuristic):
         for v in problem.get_unit_literals():
             if abs(v) not in var_assignments:
                 return abs(v), v > 0
+        lit_weights = {}
         for c in problem.get_clauses().values():
             c_weight = 2 ** -len(c)
             for lit in c:
-                if lit in self.lit_weights:
-                    self.lit_weights[lit] += c_weight
+                if lit in lit_weights:
+                    lit_weights[lit] += c_weight
                 else:
-                    self.lit_weights[lit] = c_weight
-        if len(self.lit_weights):
-            l = max(self.lit_weights, key=lambda x: self.lit_weights.get(x) if abs(x) not in var_assignments else 0)
+                    lit_weights[lit] = c_weight
+        if len(lit_weights):
+            l = max(lit_weights, key=lambda x: lit_weights.get(x) if abs(x) not in var_assignments else 0)
             return abs(l), l > 0
         raise Heuristic.no_var_left_exception
--- sat/heuristic/two_sided_jeroslow_wang_heuristic.py
+++ sat/heuristic/two_sided_jeroslow_wang_heuristic.py
@@ -3,7 +3,6 @@ from util.sat_problem import SATProblem
 
 
 class TwoSidedJeroslowWangHeuristic(Heuristic):
-    var_counts = {}
 
     def name(self) -> str:
         return "2_sided_jw"
@@ -12,17 +11,18 @@ class TwoSidedJeroslowWangHeuristic(Heuristic):
         for v in problem.get_unit_literals():
             if abs(v) not in var_assignments:
                 return abs(v), v > 0
+        var_counts = {}
         for c in problem.get_clauses().values():
             c_weight = 2 ** -len(c)
             for lit in c:
                 var = abs(lit)
-                if var in self.var_counts:
-                    self.var_counts[var][lit] += c_weight
+                if var in var_counts:
+                    var_counts[var][lit] += c_weight
                 else:
-                    self.var_counts[var] = {}
-                    self.var_counts[var][lit] = c_weight
-                    self.var_counts[var][-lit] = 0
-        if len(self.var_counts):
-            l = max(self.var_counts, key=lambda x: sum(self.var_counts[x].values()) if x not in var_assignments else 0)
-            return abs(l), max(self.var_counts[l], key=self.var_counts[abs(l)].get) > 0
+                    var_counts[var] = {}
+                    var_counts[var][lit] = c_weight
+                    var_counts[var][-lit] = 0
+        if len(var_counts):
+            l = max(var_counts, key=lambda x: sum(var_counts[x].values()) if x not in var_assignments else 0)
+            return abs(l), max(var_counts[l], key=var_counts[abs(l)].get) > 0
         raise Heuristic.no_var_left_exception
