707e7cd9eae8a83324d44f21fa777211c6014c9f
b0ster <bosterenborg@gmail.com>
2021-11-19 16:22:24 +0100

    Making it on instance level

--- sat/heuristic/one_sided_jeroslow_wang_heuristic.py
+++ sat/heuristic/one_sided_jeroslow_wang_heuristic.py
@@ -3,21 +3,20 @@ from util.sat_problem import SATProblem
 
 
 class OneSidedJeroslowWangHeuristic(Heuristic):
+    lit_weights = {}
 
     def name(self) -> str:
         return "1_sided_jw"
 
     def select(self, problem: SATProblem, var_assignments: dict[int, bool]):
-        lit_weights = {}
         for c in problem.get_clauses().values():
             c_weight = 2 ** -len(c)
             for lit in c:
-                if abs(lit) not in var_assignments:
-                    if lit in lit_weights:
-                        lit_weights[lit] += c_weight
-                    else:
-                        lit_weights[lit] = c_weight
-        if len(lit_weights):
-            l = max(lit_weights, key=lambda x: lit_weights.get(x))
+                if lit in self.lit_weights:
+                    self.lit_weights[lit] += c_weight
+                else:
+                    self.lit_weights[lit] = c_weight
+        if len(self.lit_weights):
+            l = max(self.lit_weights, key=lambda x: self.lit_weights.get(x) if abs(x) not in var_assignments else -1)
             return abs(l), l > 0
         raise Heuristic.no_var_left_exception
--- sat/heuristic/two_sided_jeroslow_wang_heuristic.py
+++ sat/heuristic/two_sided_jeroslow_wang_heuristic.py
@@ -3,24 +3,25 @@ from util.sat_problem import SATProblem
 
 
 class TwoSidedJeroslowWangHeuristic(Heuristic):
+    var_counts: dict[int, dict[int, int]] = {}
 
     def name(self) -> str:
         return "2_sided_jw"
 
     def select(self, problem: SATProblem, var_assignments: dict[int, bool]) -> tuple[int, bool]:
-        var_counts: dict[int, dict[int, int]] = {}
         for c in problem.get_clauses().values():
             c_weight = 2 ** -len(c)
             for lit in c:
                 var = abs(lit)
                 if var not in var_assignments:
-                    if var in var_counts:
-                        var_counts[var][lit] += c_weight
+                    if var in self.var_counts:
+                        self.var_counts[var][lit] += c_weight
                     else:
-                        var_counts[var] = {}
-                        var_counts[var][lit] = c_weight
-                        var_counts[var][-lit] = 0
-        if len(var_counts):
-            l = max(var_counts, key=lambda x: sum(var_counts[x].values()))
-            return (abs(l), (max(var_counts[l], key=var_counts[abs(l)].get) > 0))
+                        self.var_counts[var] = {}
+                        self.var_counts[var][lit] = c_weight
+                        self.var_counts[var][-lit] = 0
+        if len(self.var_counts):
+            l = max(self.var_counts,
+                    key=lambda x: sum(self.var_counts[x].values()) if abs(x) not in var_assignments else -1)
+            return abs(l), max(self.var_counts[l], key=self.var_counts[l].get) > 0
         raise Heuristic.no_var_left_exception
